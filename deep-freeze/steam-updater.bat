2af709f0ea5ba8bc1a99c559ad45550bf9c268f0
blastoise186 <40033667+blastoise186@users.noreply.github.com>
2020-07-25 15:50:22 +0100

    Getting the base in

--- deep-freeze/steam-updater.bat
+++ deep-freeze/steam-updater.bat
@@ -1,1 +1,10 @@
-@echo off
+Echo Giving time for Maintenance Mode to activate. One moment please.
+Echo Waiting 60 seconds for the Cloud Agent to put the other products into Maintenance Mode, since Deep Freeze will just storm through this without being patient!
+SLEEP 60
+ECHO Ok! That's hopefully enough time. Launching Steam now and allowing five minutes to update.
+cd "C:\Program Files\Steam\"
+start "" /b steam.exe
+timeout /T 300 /nobreak >nul
+taskkill /IM steam.exe /F
+ECHO I think that's enough time, so I've quit Steam for now. This task will end once Deep Freeze is ready to retake control. Goodbye!
+SLEEP 90
