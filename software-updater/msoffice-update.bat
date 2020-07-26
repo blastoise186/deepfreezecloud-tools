b22416bb4147065b788f854a5815a776fc7fe8df
blastoise186 <40033667+blastoise186@users.noreply.github.com>
2020-07-26 19:29:09 +0100

    Update msoffice-update.bat

--- software-updater/msoffice-update.bat
+++ software-updater/msoffice-update.bat
@@ -1,4 +1,4 @@
 "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user
 ECHO Hi there! I'm currently attempting to update Microsoft Office for you, so you might see a progress bar soon.
-sleep 300
+SLEEP 300
 ECHO Ok. Hopefully that should have made sure Microsoft Office has started to update. So I'm heading off for now. Goodbye!
