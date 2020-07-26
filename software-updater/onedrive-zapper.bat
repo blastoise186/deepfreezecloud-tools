27706ae0ed8433723cd60523662ab38d9cbb2770
blastoise186 <40033667+blastoise186@users.noreply.github.com>
2020-07-26 19:50:23 +0100

    Created OneDrive Zapper

---
+++ software-updater/onedrive-zapper.bat
@@ -0,0 +1,9 @@
+REM DO NOT USE THIS YET!!!
+taskkill /f /im OneDrive.exe
+%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
+rd "%UserProfile%\OneDrive" /Q /S
+rd "%LocalAppData%\Microsoft\OneDrive" /Q /S
+rd "%ProgramData%\Microsoft OneDrive" /Q /S
+rd "C:\OneDriveTemp" /Q /S
+REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
