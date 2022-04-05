64d8d9eb1ccbcc29b559436664247de97a386944
blastoise186 <40033667+blastoise186@users.noreply.github.com>
2022-04-05 12:36:36 +0100

    Create an automated silent MS Office Update script

---
+++ software-updater/msoffice-update-silent.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user displaylevel=false forceappshutdown=true
+ECHO Hi there! I'm updating Microsoft Office silently right now, so you may see the apps quit. Please save your work immediately. Thanks!
+SLEEP 300
